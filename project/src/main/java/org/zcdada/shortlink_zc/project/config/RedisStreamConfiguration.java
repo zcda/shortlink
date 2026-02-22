bb11e19864e6d76c557551f8fcca44b7e2415b69
张程 <2864923483@qq.com>
2026-02-23 00:24:51 +0800

    optimize:统一命名,讲配置文件中名字改为统一命名

--- project/src/main/java/org/zcdada/shortlink_zc/project/config/RedisStreamConfiguration.java
+++ project/src/main/java/org/zcdada/shortlink_zc/project/config/RedisStreamConfiguration.java
@@ -22,6 +22,9 @@ import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import static org.zcdada.shortlink_zc.project.common.constant.RedisKeyConstant.SHORT_LINK_STATS_STREAM_GROUP_KEY;
+import static org.zcdada.shortlink_zc.project.common.constant.RedisKeyConstant.SHORT_LINK_STATS_STREAM_TOPIC_KEY;
+
 /**
  * Redis Stream 消息队列配置
  * 公众号：马丁玩编程，回复：加群，添加马哥微信（备注：link）获取项目资料
@@ -33,10 +36,6 @@ public class RedisStreamConfiguration {
     private final RedisConnectionFactory redisConnectionFactory;
     private final ShortLinkStatsSaveConsumer shortLinkStatsSaveConsumer;
 
-    @Value("${spring.data.redis.channel-topic.short-link-stats}")
-    private String topic;
-    @Value("${spring.data.redis.channel-topic.short-link-stats-group}")
-    private String group;
 
     @Bean
     public ExecutorService asyncStreamConsumer() {
@@ -70,8 +69,8 @@ public class RedisStreamConfiguration {
                         .build();
         StreamMessageListenerContainer<String, MapRecord<String, String, String>> streamMessageListenerContainer =
                 StreamMessageListenerContainer.create(redisConnectionFactory, options);
-        streamMessageListenerContainer.receiveAutoAck(Consumer.from(group, "stats-consumer"),
-                StreamOffset.create(topic, ReadOffset.lastConsumed()), shortLinkStatsSaveConsumer);
+        streamMessageListenerContainer.receiveAutoAck(Consumer.from(SHORT_LINK_STATS_STREAM_GROUP_KEY, "stats-consumer"),
+                StreamOffset.create(SHORT_LINK_STATS_STREAM_TOPIC_KEY, ReadOffset.lastConsumed()), shortLinkStatsSaveConsumer);
         return streamMessageListenerContainer;
     }
 }
--- project/src/main/java/org/zcdada/shortlink_zc/project/mq/producer/ShortLinkStatsSaveProducer.java
+++ project/src/main/java/org/zcdada/shortlink_zc/project/mq/producer/ShortLinkStatsSaveProducer.java
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Component;
 
 import java.util.Map;
 
+import static org.zcdada.shortlink_zc.project.common.constant.RedisKeyConstant.SHORT_LINK_STATS_STREAM_TOPIC_KEY;
+
 /**
  * 短链接监控状态保存消息队列生产者
  */
@@ -17,13 +19,12 @@ public class ShortLinkStatsSaveProducer {
 
     private final StringRedisTemplate stringRedisTemplate;
 
-    @Value("${spring.data.redis.channel-topic.short-link-stats}")
-    private String topic;
+
 
     /**
      * 发送延迟消费短链接统计
      */
     public void send(Map<String, String> producerMap) {
-        stringRedisTemplate.opsForStream().add(topic, producerMap);
+        stringRedisTemplate.opsForStream().add(SHORT_LINK_STATS_STREAM_TOPIC_KEY, producerMap);
     }
 }
