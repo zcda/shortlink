a7f0a9182f3c4af6b70f564bfaa95392d96e532d
张程 <2864923483@qq.com>
2026-02-24 19:24:45 +0800

    optimize:优化无用变量

--- admin/src/main/java/org/zcdada/shortlink_zc/admin/common/biz/user/UserFlowRiskControlFilter.java
+++ admin/src/main/java/org/zcdada/shortlink_zc/admin/common/biz/user/UserFlowRiskControlFilter.java
@@ -43,7 +43,7 @@ public class UserFlowRiskControlFilter implements Filter {
         redisScript.setScriptSource(new ResourceScriptSource(new ClassPathResource(USER_FLOW_RISK_CONTROL_LUA_SCRIPT_PATH)));
         redisScript.setResultType(Long.class);
         String username = Optional.ofNullable(UserContext.getUsername()).orElse("other");
-        Long result = null;
+        Long result;
         try {
             result = stringRedisTemplate.execute(redisScript, Lists.newArrayList(username), userFlowRiskControlConfiguration.getTimeWindow());
         } catch (Throwable ex) {
